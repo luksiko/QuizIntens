c55b48845418741dc8ca55d99ed2923825d3a7b2
Aleksey <luksiko90@gmail.com>
2020-05-15 10:57:53 +0500

    Lesson_01(_homeWork)

--- script/script.js
+++ script/script.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
   'use strict';
-  let btnOpenModal = document.getElementById('btnOpenModal'),
-    modalBlock = document.getElementById('modalBlock'),
-    questionTitle = document.getElementById('question'),
-    formAnswers = document.getElementById('formAnswers'),
-    closeModal = document.getElementById('closeModal');
+  const btnOpenModal = document.getElementById('btnOpenModal'),
+        modalBlock = document.getElementById('modalBlock'),
+        questionTitle = document.getElementById('question'),
+        formAnswers = document.getElementById('formAnswers'),
+        closeModal = document.getElementById('closeModal');
 
   btnOpenModal.addEventListener('click', () => {
     modalBlock.classList.add('d-block');
@@ -16,19 +16,21 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const playTest = () => {
     const renderQuestions = () => {
+      let burgerImg = './image/burger.png',
+        burgerName = 'Стандарт';
       questionTitle.textContent = 'Какого цвета бургер вы хотите?';
       formAnswers.innerHTML = `
       <div class="answers-item d-flex flex-column">
-      <input type="radio" id="answerItem1" name="answer" class="d-none">
-      <label for="answerItem1" class="d-flex flex-column justify-content-between">
-        <img class="answerImg" src="./image/burger.png" alt="burger">
-        <span>Стандарт</span>
-      </label>
-    </div>
-    `;
+        <input type="radio" id="answerItem1" name="answer" class="d-none">
+        <label for="answerItem1" class="d-flex flex-column justify-content-between">
+          <img class="answerImg" src="${burgerImg}" alt="burger">
+          <span>${burgerName}</span>
+        </label>
+      </div>
+     `;
     };
     renderQuestions();
   };
 
 
-})
+});
